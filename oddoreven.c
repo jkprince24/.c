156327d095e6386aeef21c99be7e8a94925c6d2c
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-01-31 16:21:19 +0530

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a% 2==0);
+printf("the no %d is even");
+else
+printf("the no%dis odd");
+}
