a40921674bb3ed5bffb2e22c5942627a6aa79602
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-01-31 16:51:23 +0530

    leap year

---
+++ leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a&==0)
+{
+print("the leap year is");
+}
+else
+{
+printf("the year is not a leap year");
+}
+return0;
+}
