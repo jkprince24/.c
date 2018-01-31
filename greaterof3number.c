9a003cda72bbd6bb25f2ee424757fff09631e274
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-01-31 16:37:36 +0530

    greater of 3 number

---
+++ greaterof3number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("a is greater");
+}else if((b>c)&&(b>a))
+{
+printf("b is greater");
+}
+else
+printf("c is greater");
+return 0;
+}
