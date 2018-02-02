abbe42ba54419731b3e3999c59516eab6d76da5e
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-02-02 13:18:12 +0530

    positive or negative

---
+++ positiveornegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("enter the number",a);
+scanf("%d",&a);
+if(a>0)
+printf("the positive no is");
+elseif(a<0)
+printf("the negative number is");
+else
+printf("the no is zero");
+return 0;
+}
