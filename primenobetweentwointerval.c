d006517be1e7ec4f932ec62a0c7b9b9958a8d82d
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-02-19 16:04:18 +0530

    prime between to interval

---
+++ primenobetweentwointerval.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+{
+int n,a,b,i,count;
+printf("enter the two number");
+scanf("%d%d",&a,&b);
+for(i=a;i<b;i++)
+{
+count=0
+for(n=2;n<i;n++)
+{
+if(n=2;n,i;i++)
+{
+if(i%n=0)
+count++;
+}
+if(count==0)
+printf("%d\n",i);
+}
+}
