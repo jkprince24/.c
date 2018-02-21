83a6150b165adbd8f42177ceed8f2cf894d4f4b5
jkprince24 <35687681+jkprince24@users.noreply.github.com>
2018-02-21 16:03:29 +0530

    n number

---
+++ nnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,i;
+printf("enter the number:");
+scanf("%d",&a);
+for(n=1;i<=n;i++);
+{
+printf("the value is %d*%d=%d");
+}
+return 0;
+}
